7e9031cb4c06993299bf30a949d6f6c3ce0ee3d8
Aayan-Ismail <mohammed143ismail@gmail.com>
2026-01-31 18:00:46 +0000

    Add files via upload

---
+++ nested_dictionary.py
@@ -0,0 +1,28 @@
+student_database = {
+    'Student1':{
+        'prénom':'Aayan',
+        'surname':'ismail',
+        'age':'fifteen',
+        'marks':{
+            'maths':'98',
+            'english':'90',
+            'irish':'65',
+            'latin':'95',
+            'german':'73',
+            'french':'70',
+            'science':'90'
+        }
+
+    },
+    'student2':{
+        'prénom':'hugh',
+        'surname':'cunnigham',
+        'age':'fifteen',
+        'marks':{
+            'maths':'60'
+
+        }
+
+      },
+
+}
