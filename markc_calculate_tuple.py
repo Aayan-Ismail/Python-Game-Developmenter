fbbb5c975cf3dd9b9780eff20ba818094522ae56
Aayan-Ismail <mohammed143ismail@gmail.com>
2026-04-04 17:26:56 +0100

    Add files via upload

---
+++ markc_calculate_tuple.py
@@ -0,0 +1,50 @@
+import pgzrun
+
+student1_data = (99,40,60,75,20,38,13,52,21)
+
+maths, english, irish, science, geography, history, french, german, latin, = student1_data
+
+if maths < 40:
+    print("failed maths",print(maths))
+else:
+    print("passed maths", print(maths))
+
+if english < 40:
+    print("failed english",print(english))
+else:
+    print("passed english", print(english))
+
+if irish < 40:
+    print("failed irish",print(irish))
+else:
+    print("passed irish", print(irish))
+
+if science < 40:
+    print("failed science",print(science))
+else:
+    print("passed science", print(science))
+
+if geography < 40:
+    print("failed geography",print(geography))
+else:
+    print("passed geograhy", print(geography))
+
+if history < 40:
+    print("failed history",print(history))
+else:
+    print("passed history", print(history))
+
+if french < 40:
+    print("failed french",print(french))
+else:
+    print("passed french", print(french))
+
+if german < 40:
+    print("failed german",print(german))
+else:
+    print("passed german", print(german))
+
+if latin < 40:
+    print("failed latin",print(latin))
+else:
+    print("passed latin", print(latin))
