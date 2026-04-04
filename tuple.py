61b473454955b7b51e767ef08e6a2d8fa5f8f2c0
Aayan-Ismail <mohammed143ismail@gmail.com>
2026-04-04 17:25:58 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,7 @@
+addresss = (227,"Evil Apartment", "earth", "Eurasian Tectonic plate", "36786")
+for x in addresss:
+    print(x,end=" ")
+
+apartment_number, apartment_name, city, state, password = addresss
+
+print("\n", apartment_number, "\n", apartment_name, "\n", city, "\n", state, "\n", password)
