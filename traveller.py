11a7b0af7677f916aed4096c47fc2c2c5eea4293
Aayan-Ismail <mohammed143ismail@gmail.com>
2026-04-04 17:58:22 +0100

    Add files via upload

---
+++ traveller.py
@@ -0,0 +1,6 @@
+travel_data = (
+    ('paris', ('eiffel tower', 'bageutte','spring','not get scammed','learn french','arc de triomphe',)),
+    ('dublin',('howth','spice bag','summer', 'dont go to northside', 'guiness pub tourist trap, dont go', 'guiness storehouse')),
+    ('dubai',('poop khalfia', 'dubai chocolate','summer','dont go outside city, might see workers','cant even travel israel','marketing star hotel'))
+    ('shanghai',('bottle opener', 'noodles', 'winter','social credit score, be careful','dont say anything about tiananmen square', 'yu garden'))
+)
